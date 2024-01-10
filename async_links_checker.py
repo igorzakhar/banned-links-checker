c815b33701333c1ea8f4f80b64cd51b46fb11ba9
Igor <igorzakhar@gmail.com>
2024-01-10 12:53:27 +0300

    Add single article check

--- async_links_checker.py
+++ async_links_checker.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import sys
 
 from textwrap import shorten
 from urllib.parse import unquote
@@ -26,23 +27,28 @@ async def read_file(filename):
         return content.split('\n')
 
 
-async def fetch_articles(session, rss_url):
+async def fetch_article_urls(session, rss_url):
     async with session.get(rss_url) as response:
         resp_body = await response.text()
         feed = feedparser.parse(resp_body)
-        articles = [
-            (entry['id'], entry['title'])
+        article_urls = [
+            entry['id']
             for entry in feed.entries
         ]
-        return articles
+
+        return article_urls
 
 
-async def extract_links(session, article):
-    url, title = article
-    async with session.get(url) as response:
+async def extract_links(session, article_url):
+    async with session.get(article_url) as response:
         resp_body = await response.text()
         soup = BeautifulSoup(resp_body, 'lxml')
         post_content_body = soup.find('div', {'id': 'post-content-body'})
+
+        title = None
+        title_tag = soup.find('h1', {'class': 'tm-title'})
+        if title_tag:
+            title = title_tag.text
 
         found_links = None
         if post_content_body:
@@ -53,16 +59,16 @@ async def extract_links(session, article):
             ]
 
         return {
-            'url': url,
+            'url': article_url,
             'title': title,
             'found_links': found_links
         }
 
 
-async def collect_hyperlinks(session, articles):
+async def collect_hyperlinks(session, article_urls):
     tasks = [
-        extract_links(session, article) for article in articles
-        if not 'companies' in urlparse(article[0]).path.split('/')
+        extract_links(session, article_url) for article_url in article_urls
+        if not 'companies' in urlparse(article_url).path.split('/')
     ]
 
     return await asyncio.gather(*tasks)
@@ -108,30 +114,38 @@ async def main():
     allowed_tld = await read_file('allowed_domains/tld.txt')
     allowed_domains = await read_file('allowed_domains/domains.txt')
 
+    single_article_url = None
+    if len(sys.argv) > 1:
+        single_article_url = sys.argv[1]
+
     filename = 'article_links.txt'
     rss_url = 'https://habr.com/ru/rss/articles/?fl=ru'
 
+    found_urls = []
     async with aiohttp.ClientSession() as session:
-        articles = await fetch_articles(session, rss_url)
+        if single_article_url:
+            links = await extract_links(session, single_article_url)
+            found_urls.append(links)
+        else:
+            article_urls = await fetch_article_urls(session, rss_url)
 
-        previous_links = await read_file(filename)
-        current_links = [item[0] for item in articles]
-        unread = list(set(current_links).difference(set(previous_links)))
+            previous_links = await read_file(filename)
+            unread = list(set(article_urls).difference(set(previous_links)))
 
-        logging.debug(previous_links)
-        logging.debug(list(unread))
+            logging.debug(previous_links)
+            logging.debug(list(unread))
 
-        if unread:
-            await write_file(filename, current_links)
+            if unread:
+                await write_file(filename, article_urls)
 
-            unread_articles = [
-                article
-                for article in articles
-                if article[0] in unread
-            ]
-            results = await collect_hyperlinks(session, unread_articles)
+                unread_articles = [
+                    article_url
+                    for article_url in article_urls
+                    if article_url in unread
+                ]
+                found_urls = await collect_hyperlinks(session, unread_articles)
 
-            print_results(results, allowed_tld, allowed_domains)
+        print_results(found_urls, allowed_tld, allowed_domains)
 
 
 if __name__ == '__main__':
